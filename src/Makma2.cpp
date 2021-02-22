d6488c14d54f05e885fc17b58635c572229f52ea
Lut99 <mullertim99@gmail.com>
2021-02-22 21:47:03 +0100

    Created some file structure, setup basic CMake

---
+++ src/Makma2.cpp
@@ -0,0 +1,33 @@
+/* MAKMA 2.cpp
+ *   by Lut99
+ *
+ * Created:
+ *   2/22/2021, 3:57:00 PM
+ * Last edited:
+ *   2/22/2021, 5:31:08 PM
+ * Auto updated?
+ *   Yes
+ *
+ * Description:
+ *   Main file of the Makma2 game engine, interfaced as a library.
+**/
+
+#include <CppDebugger.hpp>
+
+using namespace std;
+using namespace CppDebugger::SeverityValues;
+
+
+/***** MANAGEMENT FUNCTIONS *****/
+/* Initializes the library and necessary data structures. */
+void init() {
+    
+}
+
+int main() {
+    DENTER("main");
+
+    DLOG(info, "Hello, world!");
+
+    DRETURN 0;
+}
